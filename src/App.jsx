65f85393e8b6f10b1bf91c39564088f6feebe05d
Alex Zapata <alexzapata1984@gmail.com>
2025-03-27 12:01:12 +0100

    Add Counter & ToggleButton components. Using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Card from "./components/Card"
 import './App.css'
+import Card from "./components/Card"
+import Counter from "./components/Counter";
+import ToggleButton from './components/ToggleButton';
 
 function App() {
   const items = ["React", "JavaScript", "Vite"];
 
   return (
-    <section>
+    <>
       <h1>¡Hola Mundo!</h1>
       <Card title="Card 1" description="Esta es una descripción" />
       <Card title="Card 2" description="Esta es otra descripción" />
+      <Card title="Card 3" description="Esta es la descripción de la card 3" />
       <ul>
         {
           items.map((item, index) => (
@@ -16,8 +19,13 @@ function App() {
           ))
         }
       </ul>
-    </section>
+
+      <h2>Contador</h2>
+      <Counter />
+      <ToggleButton />
+
+    </>
   );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = ({title, description}) => {
     return (
         <section>
             <h2>{title}</h2>
-            <img src="https://via.placeholder.com/150" alt="placeholder" />            
+            <img src="https://placehold.co/150x150" alt="placeholder" />            
             <p>{description}</p>
         </section>        
     );
